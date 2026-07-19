fb4c3ed140cd7aa10e74294b64bd4367189cbff7
alexv <alexvconcas@gmail.com>
2026-07-19 09:37:08 +0200

    Inizializzazione temporanea per visualizzare le pagine ed effettuare le prove per le pagine. Sposteremo poi tutto nelle apposite cartelle.

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+
+@app.get("/")
+def get_insert_import():
+    return render_template("insert_import.html")
+
+
+@app.post("/sumbit_import")
+def sumbit_import():
+    money = request.form.get("money", "").strip()
+    pass
