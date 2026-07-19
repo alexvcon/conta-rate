fde2e30b4ecc6d2a64ef6190f67c5e27d892281a
alexv <alexvconcas@gmail.com>
2026-07-19 09:37:56 +0200

    Creazione classe Config che prende i dati dal .env

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-import sqlite3
+import os
 
-def get_db_connection():
-    pass
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DATABASE = os.getenv("DATABASE", "database/test_db.sqlite3")
+    DEBUG = os.getenv("DEBUG", "False").lower() == "true"
